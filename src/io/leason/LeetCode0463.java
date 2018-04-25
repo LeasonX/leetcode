3d3af210a55883ba1301b43d1dc7f558b6bf8276
leason <xu.liu@1hai.cn>
2018-04-25 19:15:39 +0800

    [a] 2018-04-25

---
+++ src/io/leason/LeetCode0463.java
@@ -0,0 +1,39 @@
+package io.leason;
+
+/**
+ * https://leetcode-cn.com/problems/island-perimeter/description/
+ * <p>
+ * 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。岛屿中没有“湖”（“湖”
+ * 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+ * <p>
+ * 示例 :
+ * <p>
+ * [[0,1,0,0], [1,1,1,0], [0,1,0,0], [1,1,0,0]]
+ * <p>
+ * 答案: 16 解释: 它的周长是下面图片中的 16 个黄色的边：
+ */
+public class LeetCode0463 {
+
+    public int islandPerimeter(int[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    result += 4;
+                    if (i != grid.length - 1) {
+                        if (grid[i + 1][j] == 1) {
+                            result -= 2;
+                        }
+                    }
+                    if (j != grid[0].length - 1) {
+                        if (grid[i][j + 1] == 1) {
+                            result -= 2;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+}
